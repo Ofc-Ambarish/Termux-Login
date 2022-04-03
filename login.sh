ded6d4b256d620c9a08781d7ccbabca696573b0b
Ambarish Mandal <94108973+Ofc-Ambarish@users.noreply.github.com>
2022-04-03 18:23:27 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -32,3 +32,61 @@ read -s -p $'       \e[32m\033[1m\033[33m[\033[31m+\033[33m] \033[37mINPUT \033[
 if [[ \$pass == $password && \$user == $username ]]; then
 sleep 3
 clear
+cd $HOME
+cd Termux-Login
+cd Song
+python sound_effect.py
+clear
+cd $HOME 
+echo -e "\033[1m\033[33m
+
+██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗ 
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+███████║███████║██║     █████╔╝ █████╗  ██████╔╝
+██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ 
+"
+echo -e  "     \e[1m\e[32m▂▃▄▅▆▇▓▒░ \033[1mCoded By \e[33mM4ND33P \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+cd /sdcard
+python network.py
+echo -e "   \033[1m\033[33m]\033[31m─────────────────────────────────────\033[33m["
+echo 
+PS1='\033[1m\[\e[32m\]\033[1m┌─[\[\e[37m\]\T\[\e[32m\]\033[1m]─────\033[1m\e[1;98m\[[\033[1m\033[37m$names\033[32m]\033[1m\e[0;32m\033[1m───[\033[38;5;209m\#\033[32m]\n|\n\033[1m\e[0;32m\033[1m└─[\[\e[32m\]\e[1;33m\W\[\e[1m\033[32m]\033[1m────►\e[1;93m\033[1m '
+<< comment
+shopt -s autocd
+shopt -s cdspell
+shopt -s checkhash
+shopt -s checkwinsize
+shopt -s compat31
+shopt -s compat32
+shopt -s compat40
+shopt -s compat41
+shopt -s no_empty_cmd_completion
+shopt -s histverify
+shopt -s histappend
+shopt -s dirspell
+shopt -s direxpand
+shopt -s compat43
+shopt -s compat32
+shopt -s lithist
+comment
+cd $HOME
+cd Termux-Login
+cd
+else
+echo ""
+echo -e "\e[1;31m  Your Password Is Incorrect ! <)
+\e[0m"
+sleep 1
+cmatrix -L
+fi
+trap 2
+LOGIN
+echo 
+echo
+echo 
+echo -e "\033[1m\e[1;32m     Your Termux is \033[33mReady \n
+       So please \033[31mExit \033[37mand \033[32mLogin.\e[0m"
+echo
+echo
